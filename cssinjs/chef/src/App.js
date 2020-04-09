e2260bf4fa8b8618f15387ce34856ad002792188
Oleg Isonen <oleg008@gmail.com>
2020-04-09 13:28:58 +0200

    remove sc from the bundle

--- cssinjs/chef/src/App.js
+++ cssinjs/chef/src/App.js
@@ -1,13 +1,10 @@
 import * as React from 'react';
-import {ThemeProvider as ScThemeProvider} from 'styled-components';
-import {ThemeProvider as JssThemeProvider} from 'react-jss';
+import {ThemeProvider} from 'react-jss';
 import {Player} from './Player';
 import {theme} from './design-system/theme';
 
 export const App = () => (
-  <ScThemeProvider theme={theme}>
-    <JssThemeProvider theme={theme}>
-      <Player />
-    </JssThemeProvider>
-  </ScThemeProvider>
+  <ThemeProvider theme={theme}>
+    <Player />
+  </ThemeProvider>
 );
