2789e4c3017357fd19712a5c4af750f6f695d358
Oleg Isonen <oleg008@gmail.com>
2020-04-09 13:26:34 +0200

    refactorr ciruclar progress

--- cssinjs/chef/src/design-system/CircularProgress.js
+++ cssinjs/chef/src/design-system/CircularProgress.js
@@ -1,14 +1,27 @@
-// Based on https://github.com/kevinsqi/react-circular-progressbar
-
-import React from 'react';
-import styled from 'styled-components';
+import * as React from 'react';
+import {createUseStyles, useTheme} from 'react-jss';
 
 const VIEWBOX_SIZE = 100;
 const STROKE_WIDTH = 6;
 const CIRCLE_RATIO = 1;
 
-function getDashStyle({dashRatio, radius}) {
-  const diameter = Math.PI * 2 * radius;
+// The radius of the path is defined to be in the middle, so for the path to
+// fit perfectly inside the 100x100 viewBox, need to subtract half the strokeWidth
+const PATH_RADIUS = VIEWBOX_SIZE / 2 - STROKE_WIDTH / 2;
+
+// Move to center of canvas
+// Relative move to top canvas
+// Relative arc to bottom of canvas
+// Relative arc to top of canvas
+const PATH = `
+  M ${VIEWBOX_SIZE / 2},${VIEWBOX_SIZE / 2}
+  m 0,-${PATH_RADIUS}
+  a ${PATH_RADIUS},${PATH_RADIUS} ${0} 1 1 0,${2 * PATH_RADIUS}
+  a ${PATH_RADIUS},${PATH_RADIUS} ${0} 1 1 0,-${2 * PATH_RADIUS}
+`;
+
+const getDashStyle = (dashRatio = CIRCLE_RATIO) => {
+  const diameter = Math.PI * 2 * PATH_RADIUS;
   const gapLength = (1 - dashRatio) * diameter;
 
   return {
@@ -17,64 +30,50 @@ function getDashStyle({dashRatio, radius}) {
     // Shift dash backward by gapLength, so gap starts appearing at correct distance
     strokeDashoffset: `${gapLength}px`
   };
-}
-
-function getPath(radius) {
-  // Move to center of canvas
-  // Relative move to top canvas
-  // Relative arc to bottom of canvas
-  // Relative arc to top of canvas
-  return `
-      M ${VIEWBOX_SIZE / 2},${VIEWBOX_SIZE / 2}
-      m 0,-${radius}
-      a ${radius},${radius} ${0} 1 1 0,${2 * radius}
-      a ${radius},${radius} ${0} 1 1 0,-${2 * radius}
-    `;
-}
-
-function getPathRadius() {
-  // The radius of the path is defined to be in the middle, so for the path to
-  // fit perfectly inside the 100x100 viewBox, need to subtract half the strokeWidth
-  return VIEWBOX_SIZE / 2 - STROKE_WIDTH / 2;
-}
+};
 
 // Ratio of path length to trail length, as a value between 0 and 1
-function getPathRatio({value, min, max}) {
+const getPathRatio = ({value, min, max}) => {
   const boundedValue = Math.min(Math.max(value, min), max);
   return (boundedValue - min) / (max - min);
-}
+};
 
-const Path = ({dashRatio, radius, ...rest}) => (
+const useStyles = createUseStyles({
+  root: {
+    overflow: 'hidden'
+  },
+  progress: ({theme, ...rest}) => ({
+    stroke: theme.colors.primary,
+    transition: 'stroke-dashoffset 0.5s ease 0s',
+    ...getDashStyle(getPathRatio(rest))
+  }),
+  trail: {
+    stroke: ({theme}) => theme.colors.border,
+    strokeLinecap: 'round',
+    ...getDashStyle()
+  }
+});
+
+const Path = ({className}) => (
   <path
-    {...rest}
-    style={getDashStyle({dashRatio, radius})}
-    d={getPath(radius)}
+    className={className}
+    d={PATH}
     strokeWidth={STROKE_WIDTH}
     fillOpacity={0}
   />
 );
 
-const Image = styled.svg`
-  overflow: hidden;
-`;
-
-const Trail = styled(Path)`
-  stroke: ${(p) => p.theme.colors.border};
-  stroke-linecap: round;
-`;
-
-const Progress = styled(Path)`
-  stroke: ${(p) => p.theme.colors.primary};
-  transition: stroke-dashoffset 0.5s ease 0s;
-`;
-
 export const CircularProgress = ({value, min, max, className}) => {
-  const radius = getPathRadius();
-  const pathRatio = getPathRatio({value, min, max});
+  const theme = useTheme();
+  const classes = useStyles({theme, value, min, max});
+
   return (
-    <Image viewBox={`0 0 ${VIEWBOX_SIZE} ${VIEWBOX_SIZE}`} className={className}>
-      <Trail dashRatio={CIRCLE_RATIO} radius={radius} />
-      <Progress dashRatio={pathRatio * CIRCLE_RATIO} radius={radius} />
-    </Image>
+    <svg
+      viewBox={`0 0 ${VIEWBOX_SIZE} ${VIEWBOX_SIZE}`}
+      className={`${classes.root} ${className}`}
+    >
+      <Path className={classes.trail} />
+      <Path className={classes.progress} />
+    </svg>
   );
 };
