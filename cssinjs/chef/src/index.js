45c4e6dd91f75d8989bc03f490306b2caf872098
Oleg Isonen <oleg008@gmail.com>
2020-04-10 23:21:22 +0200

    Revert "Chef"

--- cssinjs/chef/src/index.js
+++ cssinjs/chef/src/index.js
@@ -1,15 +1,12 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import {ThemeProvider} from 'react-jss';
-import {RecipePlayer} from './screens/RecipePlayer';
-import {theme} from './theme';
-import pourover from './recipes/pourover.json';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./App";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RecipePlayer recipe={pourover} />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
