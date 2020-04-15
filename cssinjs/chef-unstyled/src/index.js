5a4749cf72e5b9d5215c697a9366d3955d26adcc
Oleg Isonen <oleg008@gmail.com>
2020-04-16 00:36:16 +0200

    add sc theme provider to unstyled version as well

--- cssinjs/chef-unstyled/src/index.js
+++ cssinjs/chef-unstyled/src/index.js
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import {ThemeProvider} from 'react-jss';
+import {ThemeProvider as JssThemeProvider} from 'react-jss';
+import {ThemeProvider as ScThemeProvider} from 'styled-components';
 import {RecipePlayer} from './screens/RecipePlayer';
 import {theme} from './theme';
 import pourover from './recipes/pourover.json';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RecipePlayer recipe={pourover} />
-    </ThemeProvider>
+    <ScThemeProvider theme={theme}>
+      <JssThemeProvider theme={theme}>
+        <RecipePlayer recipe={pourover} />
+      </JssThemeProvider>
+    </ScThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
